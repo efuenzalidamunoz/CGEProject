e971eec370622f2b4f1f182ce9473caa5e8e3695
efuenzalidamunoz <emerson.fuenzalida@alumnos.ucm.cl>
2025-11-05 11:35:25 -0300

    update main.kt

--- composeApp/src/jvmMain/kotlin/org/example/cgeproject/main.kt
+++ composeApp/src/jvmMain/kotlin/org/example/cgeproject/main.kt
@@ -1,12 +1,22 @@
 package org.example.cgeproject
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
+    val state = rememberWindowState(
+        width = 1920.dp,
+        height = 1080.dp,
+        position = WindowPosition.Aligned(Alignment.Center),
+    )
     Window(
         onCloseRequest = ::exitApplication,
-        title = "cgeproject",
+        title = "CGE",
+        state = state
     ) {
         App()
     }
