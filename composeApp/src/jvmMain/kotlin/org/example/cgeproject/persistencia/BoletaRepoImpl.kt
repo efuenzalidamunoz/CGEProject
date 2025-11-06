17ff63e4ec88c3c3d04c632fa9d40bdf9045f488
mauricio <mauricastroleal@gmail.com>
2025-11-05 22:26:26 -0300

    agregamos comentarios

--- composeApp/src/jvmMain/kotlin/org/example/cgeproject/persistencia/BoletaRepoImpl.kt
+++ composeApp/src/jvmMain/kotlin/org/example/cgeproject/persistencia/BoletaRepoImpl.kt
@@ -3,21 +3,25 @@ package org.example.cgeproject.persistencia
 import org.example.cgeproject.dominio.Boleta
 
 class BoletaRepoImpl(private val persist: PersistenciaDatos): BoletaRepositorio {
+    /** Guardamos las boletas en la implementación del repositorio **/
     override fun guardar(b: Boleta): Boleta {
         persist.guardarBoleta(b)
         return b
     }
 
+    /** Capturamos las boletas mediante el rut, año y mes de la facturación **/
     override fun obtener(rut: String, anio: Int, mes: Int): Boleta? {
         return persist.obtenerBoletas().find {
             it.getIdCliente() == rut && it.getAnio() == anio && it.getMes() == mes
         }
     }
 
+    /** Listamos los clientes que se encuentren en el repositorio con los detalles de su boleta **/
     override fun listarPorCliente(rut: String): List<Boleta> {
         return persist.obtenerBoletas().filter { it.getIdCliente() == rut }
     }
 
+    /** Se eliminan las boletas **/
     override fun eliminarBoleta(rut: String, anio: Int, mes: Int) {
         persist.eliminarBoleta(rut, anio, mes)
     }
--- composeApp/src/jvmMain/kotlin/org/example/cgeproject/persistencia/ClienteRepoImpl.kt
+++ composeApp/src/jvmMain/kotlin/org/example/cgeproject/persistencia/ClienteRepoImpl.kt
@@ -3,11 +3,13 @@ package org.example.cgeproject.persistencia
 import org.example.cgeproject.dominio.Cliente
 
 class ClienteRepoImpl(private val persist: PersistenciaDatos) : ClienteRepositorio {
+    /** Creamos a los clientes y los guardamos en el repositorio **/
     override fun crear(c: Cliente): Cliente {
         persist.guardarCliente(c)
         return c
     }
 
+    /** Actualizamos las lista **/
     override fun actualizar(c: Cliente): Cliente {
         // Eliminamos el cliente anterior y volvemos a guardar
         persist.eliminarCliente(c.getRut())
@@ -15,14 +17,17 @@ class ClienteRepoImpl(private val persist: PersistenciaDatos) : ClienteRepositor
         return c
     }
 
+    /** Eliminamos a los clientes del repositorio **/
     override fun eliminar(rut: String): Boolean {
         return persist.eliminarCliente(rut)
     }
 
+    /** Buscamos al cliente por su rut **/
     override fun obtenerPorRut(rut: String): Cliente? {
         return persist.buscarClientePorRut(rut)
     }
 
+    /** Enlistamos a los clientes **/
     override fun listar(filtro: String): List<Cliente> {
         val lista = persist.obtenerClientes()
         return if (filtro.isBlank()) lista
